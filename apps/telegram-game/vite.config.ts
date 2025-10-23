5aed544d15c4a588d105fe9524cefee70a5482d7
Nick Kosarev <hmbanan666@hotmail.com>
2025-10-23 10:27:49 +0200

    feat: updated deps structure (#544)
    * feat: updated deps structure
    
    * chore: docker updated

--- apps/telegram-game/vite.config.ts
+++ apps/telegram-game/vite.config.ts
@@ -7,13 +7,11 @@ import IconsResolver from 'unplugin-icons/resolver'
 import Icons from 'unplugin-icons/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     AutoImport({
       imports: [
         'vue',
